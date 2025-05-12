a89a01ac51e61ec7b991708baf5bf3244402c0ea
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-05-12 22:54:19 +0500

    layout for a banner

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Process from './components/Process';
 import Plans from './components/Plans';
 import Faqs from './components/Faqs';
 import Contact from './components/Contact';
+import Banner from './components/Banner';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Plans />
       <Contact />
       <Faqs />
+      <Banner />
     </div>
   )
 }
