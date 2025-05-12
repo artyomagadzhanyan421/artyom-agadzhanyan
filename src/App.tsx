ec7d14c95f1555190b09ccdb83eec84d9a3adcc6
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-05-12 14:46:46 +0500

    process section for the website

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 // Components 
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Process from './components/Process';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Process />
     </div>
   )
 }
---
+++ src/components/Process.tsx
@@ -0,0 +1,38 @@
+import "../styles/Process.css";
+
+function Process() {
+    return (
+        <div className='Process'>
+            <center>
+                <div className="sub-head">
+                    <span style={{ color: 'rgba(255, 255, 255, .7)' }}>Process</span>
+                </div>
+                <center>
+                    <p className="heading section">Your path to excellence</p>
+                </center>
+                <center>
+                    <p className="pitch">A simple, effective approach to deliver excellence.</p>
+                </center>
+                <div className="process">
+                    <div className="phase">
+                        <i className="bx bx-target-lock"></i>
+                        <h3>Discovery & Analysis</h3>
+                        <span>I dive deep into your needs, exploring ideas and defining strategies for long-term success.</span>
+                    </div>
+                    <div className="phase">
+                        <i className="bx bxs-dashboard"></i>
+                        <h3>Development & Test</h3>
+                        <span>I craft tailored solutions for your goals and rigorously test them for top-notch reliability.</span>
+                    </div>
+                    <div className="phase">
+                        <i className="bx bxs-planet"></i>
+                        <h3>Launch & Maintain</h3>
+                        <span>I deploy your solution seamlessly and ensure its continued performance with ongoing care.</span>
+                    </div>
+                </div>
+            </center>
+        </div>
+    )
+}
+
+export default Process
