1f91ee454893333a800bbb4410d93eef530820e4
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-05-12 15:04:57 +0500

    adding plans section

---
+++ src/components/Plans.tsx
@@ -0,0 +1,26 @@
+import "../styles/Plans.css";
+
+function Plans() {
+    return (
+        <div className='Process' id="plans">
+            <center>
+                <div className="sub-head">
+                    <span style={{ color: 'rgba(255, 255, 255, .7)' }}>Plans</span>
+                </div>
+            </center>
+            <center>
+                <p className="heading section">Flexible plans for growth</p>
+            </center>
+            <center>
+                <p className="pitch">Transparent pricing designed to fit your requirements.</p>
+            </center>
+            <div className="plans">
+                <div className="plan">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Plans
