f7050a6e30fe9db0bae66986ff92330d0c2c7c9f
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-05-12 12:09:34 +0500

    hero section of the website

---
+++ src/components/Hero.tsx
@@ -0,0 +1,36 @@
+import "../styles/Hero.css";
+
+function Hero() {
+    return (
+        <div className="Hero">
+            <center>
+                <div className="sub-head">
+                    <img src="/artyom.jpg" alt="image" />
+                    <span>Hi, my name is Artyom!</span>
+                </div>
+            </center>
+            <center>
+                <p className="heading">Transforming Workflows Into Interactive Websites</p>
+                <p className="pitch">I’m a front-end developer who specialized in building stylish, scalable web apps with React and TypeScript.</p>
+                <div className="links">
+                    <a href="#" className="nav-link">
+                        <span>My Services</span>
+                        <i className='bx bx-link-external' style={{ fontSize: 16 }}></i>
+                    </a>
+                    <a href="#" className="nav-link">
+                        <span>See Plans</span>
+                    </a>
+                </div>
+                <div className="stack">
+                    <i className='bx bxl-react'></i>
+                    <i className='bx bxl-typescript' ></i>
+                    <i className='bx bxl-nodejs'></i>
+                    <i className='bx bxl-mongodb' ></i>
+                    <i className='bx bxl-redux'></i>
+                </div>
+            </center>
+        </div>
+    )
+}
+
+export default Hero
