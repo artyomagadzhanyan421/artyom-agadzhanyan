b7c9d64256c5dc511e5bc6dc156fb2c51b213a52
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-05-13 01:47:43 +0500

    setting up about section

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,14 @@ import Faqs from './components/Faqs';
 import Contact from './components/Contact';
 import Banner from './components/Banner';
 import Showcase from './components/Showcase';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
       <Process />
       <Showcase />
       <Plans />
---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import "../styles/About.css";
+
+function About() {
+    return (
+        <div className='Process' id="about">
+            <center>
+                <div className="sub-head">
+                    <span style={{ color: 'rgba(255, 255, 255, .7)' }}>Who I'am?</span>
+                </div>
+                <p className="heading section" id="about-section" style={{ maxWidth: 800 }}>I help businesses like yours to automate their day to day business operations building web solutions</p>
+            </center>
+        </div>
+    )
+}
+
+export default About
