7853be4c25cc197815c486af3480ab23e3916ed1
artyomagadzhanyan421 <artyom.agadzhanyan421@gmail.com>
2025-05-15 11:50:57 +0500

    sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "../styles/Navbar.css";
 
 function Navbar() {
+    const [toggle, setToggle] = useState(false);
+
+    const toggleFunc = () => setToggle(!toggle);
+
     return (
         <div className="Navbar">
             <nav>
@@ -8,17 +13,19 @@ function Navbar() {
                     <img src="/artyom.jpg" alt="image" />
                     <span>Artyom A.</span>
                 </a>
-                <ul>
-                    <li><a href="#process">Process</a></li>
-                    <li><a href="#showcase">Showcase</a></li>
-                    <li><a href="#plans">Plans</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#faqs">FAQs</a></li>
+                <ul className={toggle ? "toggle" : ""}>
+                    <li><a href="#process" onClick={toggleFunc}>Process</a></li>
+                    <li><a href="#showcase" onClick={toggleFunc}>Showcase</a></li>
+                    <li><a href="#plans" onClick={toggleFunc}>Plans</a></li>
+                    <li><a href="#contact" onClick={toggleFunc}>Contact</a></li>
+                    <li><a href="#faqs" onClick={toggleFunc}>FAQs</a></li>
+                    <li className='touch'><a href="#" onClick={toggleFunc}>Get in touch</a></li>
                 </ul>
                 <a href="#" className="nav-link">
                     <span>Get in touch</span>
                     <i className='bx bx-link-external' style={{ fontSize: 16 }}></i>
                 </a>
+                <i className="bx bx-grid-alt" onClick={toggleFunc}></i>
             </nav>
         </div>
     )
